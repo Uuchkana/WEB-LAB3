d6a00920aaedf58db2b9b1e52fb6393c88577262
uuchkaana <uuchka.brabbit@gmail.com>
2020-06-01 22:48:47 +0800

    hello

---
+++ cs142-template-processor.js
@@ -0,0 +1,15 @@
+function Cs142TemplateProcessor (template) {
+	this.template = template;
+}
+Cs142TemplateProcessor.prototype.fillIn = function(dict) {
+	// do sth to replace properties in template with those in dictionary
+	var str = this.template;
+	var reg = /{{[^{]*}}/g;
+	var words = str.match(reg);
+	// console.log(typeof(words));
+	for(var i = 0; i < words.length; i++) {
+		var word = words[i].replace("{{", "").replace("}}", "");
+		str = str.replace(words[i], dict[word] || "");
+	}
+	return str;
+};
